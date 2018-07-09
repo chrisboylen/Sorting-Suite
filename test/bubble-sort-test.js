76e589d51448e387af9319aa1ce141cd07c50ae7
Chris Boylen <billpolster@gmail.com>
2018-07-09 17:58:48 -0600

    Add and pass test for bubbleSort with an array of 12 integers

--- test/bubble-sort-test.js
+++ test/bubble-sort-test.js
@@ -7,4 +7,13 @@ describe('bubbleSort()', () => {
 
     expect(bubbleSort).to.exist;
   })
+
+  it('Should sort an array of 11 numbers from least to greatest', () => {
+    let array = [1, 0, 11, 3, 2, 8, 10, 5, 7, 4, 9, 6];
+
+    let expectedState = array.sort();
+    let actualState = bubbleSort(array);
+
+    expect(expectedState).to.deep.equal(actualState);
+  })
 })
