50444624b86bfa42ecd879bb7cac69887c7d461c
Chris Boylen <billpolster@gmail.com>
2018-07-09 17:03:16 -0600

    Add test that bubbleSort exists

---
+++ test/bubble-sort-test.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import { expect, assert } from 'chai';
+import bubbleSort from '../lib/bubble-sort.js';
+
+describe('bubbleSort()', () => {
+  it('Should be a function', () => {
+
+    expect(bubbleSort).to.exist;
+  })
+})
